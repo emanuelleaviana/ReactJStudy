c1bf4380199d0a0e5704ca86727556f3aa06256f
Emanuelle Viana <93949785+emanuelleaviana@users.noreply.github.com>
2023-01-25 10:30:23 -0300

    first commit

---
+++ reactapp/src/components/Card/index.jsx
@@ -0,0 +1,10 @@
+import './styles.css';
+
+export function Card() {
+    return (
+        <div className='card'>
+            <strong>Emanuelle Viana</strong>
+            <small>10:15:24</small>
+        </div>
+    )
+}
