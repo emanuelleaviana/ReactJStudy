dcfadb8b25b91edd508cf3908365a9513ec957f2
Emanuelle Viana <93949785+emanuelleaviana@users.noreply.github.com>
2023-01-26 19:53:21 -0300

    addUseState

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,23 @@
+import React, { useState } from 'react';
 import './styles.css';
 import { Card } from '../../components/Card/';
 
 export function Home() {
 
+  const [studentName, setStudentName] = useState();
+
   return (
     <div className='container'>
-      <h1>Lista de presença!</h1>
-      <input type="text" placeholder='Digite o nome...' />
+      <h1>Lista de presença! {studentName}</h1>
+      <input 
+      type="text" 
+      placeholder='Digite o nome...'
+      onChange={e => setStudentName(e.target.value)} />
       <button type='button'>Adicionar</button>
 
-      <Card />
-      <Card />
-      <Card />
+      <Card name="Emanuelle"  time="10:55:25"/>
+      <Card name="Maria"  time="10:56:54"/>
+      <Card name="Carlos"  time="11:32:44"/>
     </div>
   )
 }
