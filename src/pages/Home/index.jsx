9f1223f2661d8c04b216ec90ddcc0c7e8e548d27
Emanuelle Viana <93949785+emanuelleaviana@users.noreply.github.com>
2023-01-26 20:25:04 -0300

    addFunctionMap

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,19 +5,37 @@ import { Card } from '../../components/Card/';
 export function Home() {
 
   const [studentName, setStudentName] = useState();
+  const [students, setStudents] = useState([]);
+
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      })
+    };
+    setStudents(prevState => [...prevState, newStudent]);
+  }
+
 
   return (
     <div className='container'>
-      <h1>Lista de presença! {studentName}</h1>
-      <input 
-      type="text" 
-      placeholder='Digite o nome...'
-      onChange={e => setStudentName(e.target.value)} />
-      <button type='button'>Adicionar</button>
+      <h1>Lista de presença! </h1>
+      <input
+        type="text"
+        placeholder='Digite o nome...'
+        onChange={e => setStudentName(e.target.value)} />
+      <button type='button' onClick={handleAddStudent}>
+        Adicionar
+      </button>
 
-      <Card name="Emanuelle"  time="10:55:25"/>
-      <Card name="Maria"  time="10:56:54"/>
-      <Card name="Carlos"  time="11:32:44"/>
+      {
+        students.map(student => <Card name={student.name} time={student.time} />
+        )
+      }
+
     </div>
   )
 }
